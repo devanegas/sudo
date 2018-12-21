18d77e57ad15b7736811f859084e60a8c5726557
devanegas <dieguitovan@gmail.com>
2018-12-21 16:58:29 -0700

    Started the Sudo Page

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function myFunction(){
+    var x = document.getElementById("myNav")
+
+    if(x.className === "navigation"){
+        x.className += " responsive";
+    }
+    else{
+        x.className = "navigation"
+    }
+}
